ec45a246e1ab2ecf84d5cf3a1d59d78968fa8dc4
Peđa Milaković <pedjamilakovic1@gmail.com>
2020-12-28 21:26:30 +0100

    German and English languages

--- src/main/java/net/groundzeroit/web/WebApplication.java
+++ src/main/java/net/groundzeroit/web/WebApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.mail.javamail.JavaMailSender;
-// import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
-// import java.util.Properties;
+import java.util.Locale;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
@@ -29,5 +31,29 @@ public class WebApplication {
         registrationBean.addUrlPatterns("/*");
         return registrationBean;
     }
+
+    // Adding Beans for internationalization
+    @Bean // <--- 1
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver(); // <--- 2
+        localeResolver.setDefaultLocale(Locale.US); // <--- 3
+
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        // Defaults to "locale" if not set
+        localeChangeInterceptor.setParamName("localeData");
+
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+    }
+
 
 }
